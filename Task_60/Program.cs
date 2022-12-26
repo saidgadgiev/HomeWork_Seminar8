d5bd7f36b93f755389c338ad92b73f4adab0c17b
Said Gadjiev <ackusha1984@ya.ru>
2022-12-27 00:13:08 +0300

    Решение задачи 60

--- Task_60/Program.cs
+++ Task_60/Program.cs
@@ -8,3 +8,44 @@
 27(0,0,1) 90(0,1,1)
 26(1,0,1) 55(1,1,1)
 */
+
+int GetNumber(string messege)
+{
+    int resultat;
+    while (true)
+    {
+        Console.Write(messege);
+        if (int.TryParse(Console.ReadLine(), out resultat) && resultat > 0)
+        {
+            break;
+        }
+        else Console.WriteLine("Ввели не число");
+    }
+    return resultat;
+}
+
+Console.WriteLine($"Введите размер массива X x Y x Z: ");
+int x = GetNumber("Введите X: ");
+int y = GetNumber("Введите Y: ");
+int z = GetNumber("Введите Z: ");
+
+void InitMatrix3D(int x, int y, int z)
+{
+    int[,,] matrix3D = new int[x, y, z];
+    Random rnd = new Random();
+    for (int i = 0; i < x; i++)
+    {
+        for (int j = 0; j < y; j++)
+        {
+            for (int k = 0; k < z; k++)
+            {
+                matrix3D[i,j,k] = rnd.Next(10,100);
+                Console.Write($"{matrix3D[i,j,k]}({i},{j},{k}) ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+}
+
+InitMatrix3D(x, y, z);
